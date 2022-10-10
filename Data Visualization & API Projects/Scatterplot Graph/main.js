d258a1c327a4e524a9c92754cc91913c1e4dfb51
beelzeboss92 <99251238+beelzeboss92@users.noreply.github.com>
2022-10-10 19:41:09 -0400

    Work on JS file for scatterplot graph

--- Data Visualization & API Projects/Scatterplot Graph/main.js
+++ Data Visualization & API Projects/Scatterplot Graph/main.js
@@ -3,20 +3,20 @@ var height = 600;
 
 // Tooltip element
 var tooltip = d3
-.select('.chart-container')
+.select('.sttp-graph')
 .append('div')
 .attr('id', 'tooltip');
 
 // Overlay element
 var overlay = d3
-.select('.chart-container')
+.select('.sttp-graph')
 .append('div')
 .attr('class', 'overlay')
 .style('opacity', 0);
 
 // Bar chart container element
 var svgContainer = d3
-.select('.chart-container')
+.select('.sttp-graph')
 .append('svg')
 .attr('width', width)
 .attr('height', height);
@@ -25,85 +25,42 @@ d3.json(
 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json')
 .then(data => {
 
-    // Create and rotate required text to Y axis
-    svgContainer
-        .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('x', -200)
-        .attr('y', 80)
-        .text('Gross Domestic Product');
-
-    // Create and rotate required text to X axis
-    svgContainer
-        .append('text')
-        .attr('x', width / 2 + 120)
-        .attr('y', height + 50)
-        .text('More Information: http://www.bea.gov/national/pdf/nipaguid.pdf')
-        .attr('class', 'info');
-
-    // Map and store years along with their quarters
-    var years = data.data.map(function (item) {
-        var quarter;
-        var temp = item[0].substring(5, 7);
-
-        if (temp === '01') {
-            quarter = 'Q1';
-        } else if (temp === '04') {
-            quarter = 'Q2';
-        } else if (temp === '07') {
-            quarter = 'Q3';
-        } else if (temp === '10') {
-            quarter = 'Q4';
-        }
-
-        return item[0].substring(0, 4) + ' ' + quarter;
+    // Map and parse minutes and seconds
+    var timeFormat = '%M:%S';
+    var parsedTime = data.time.map(d => {
+        return d3.timeParse(timeFormat)(d);
     });
 
-    // Store and map dates from JSON file
-    var yearsDate = data.data.map(function (item) {
-        return new Date(item[0]);
-    });
+    // Scale, set range and domain for X axis
+    var xScale = d3.scaleLinear().domain([
+        d3.min(d => {
+            return d.Year;
+        }),
+        d3.max(d => {
+            return d.Year;
+        })
+    ]).range([0, width]);
 
-    // Find and set the max date from dates stored in yearsDate
-    var xMax = new Date(d3.max(yearsDate));
-    xMax.setMonth(xMax.getMonth() + 3);
-
-    // Set domain and range for scale of the X axis
-    var xScale = d3.scaleTime()
-        .domain([d3.min(yearsDate), xMax])
-        .range([0, width]);
+    // Scale, set range and domain for Y axis
+    var yScale = d3.scaleTime().domain([
+        d3.extent(parsedTime)
+    ]).range([0, height]);
 
     // Set and store X axis in xAxis 
-    var xAxis = d3.axisBottom().scale(xScale);
+    var xAxis = d3.axisBottom(xScale)
+        .tickFormat(d3.format);
+
+    // Set and store yAxisScale to the Y axis
+    var yAxis = d3.axisLeft(yScale)
+        .tickValues(parsedTime)
+        .tickFormat(timeFormat);
 
     // Create and setup X axis elemnt
     svgContainer
         .append('g')
         .call(xAxis)
         .attr('id', 'x-axis')
-        .attr('transform', 'translate(60, 400)');
-
-    // Map and store GDP data from JSON file
-    var GDP = data.data.map(function (item) {
-        return item[1];
-    });
-
-    // Set the max GDP
-    var gdpMax = d3.max(GDP);
-
-    // Set linear scale, domain, and range for y axes using GDP range
-    var linearScale = d3.scaleLinear().domain([0, gdpMax]).range([0, height]);
-
-    // Map and store Scaled Linear GDP data from JSON file
-    var scaledGDP = GDP.map(function (item) {
-        return linearScale(item);
-    });
-
-    // Set and store scaled GDP data for Y axis
-    var yAxisScale = d3.scaleLinear().domain([0, gdpMax]).range([height, 0]);
-
-    // Set and store yAxisScale to the Y axis
-    var yAxis = d3.axisLeft(yAxisScale);
+        .attr('transform', 'translate(0,' + height + ')');
 
     // Create Y axis element for chart
     svgContainer
@@ -112,41 +69,38 @@ d3.json(
         .attr('id', 'y-axis')
         .attr('transform', 'translate(60, 0)');
 
-    // Section for bars to be created and added
-    d3.select('svg')
-        .selectAll('rect')
-        .data(scaledGDP)
-        .enter()
-        .append('rect')
-        // Give each bar element an attribute to store their own dates
-        .attr('data-date', function (d, i) {
-            return data.data[i][0];
-        })
-        // Give each bar element an attribute to store their GDP amount
-        .attr('data-gdp', function (d, i) {
-            return data.data[i][1];
-        })
-        .attr('class', 'bar')
-        // Set each bar's position on the X axis
-        .attr('x', function (d, i) {
-            return xScale(yearsDate[i]);
-        })
-        // Set each bar's position on the Y axis
-        .attr('y', function (d) {
-            return height - d;
-        })
-        .attr('width', barWidth)
-        .attr('height', function (d) {
-            return d;
-        })
-        // Give and assign an index attr for each element for the tooltip
-        .attr('index', (d, i) => i)
-        .style('fill', '#33adff')
-        .attr('transform', 'translate(60, 0)')
+    // Add and rotate text element to Y axis
+    svgContainer
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('x', -160)
+        .attr('y', -44)
+        .text('Time in Minutes')
 
+    // Add dots to scatterplot
+    svgContainer
+        .selectAll('.dot')
+        .data(data)
+        .enter()
+        .attr('class', 'dot')
+        .attr('r', 6)
+        .attr('cx', d => {
+            return xScale(d.Year);
+        })
+        .attr('cy', d => {
+            return yScale(d.Time);
+        })
+        .attr('data-xvalue', d => {
+            return d.Year;
+        })
+        .attr('data-yvalue', d => {
+            return d.Time.toISOString();
+        })
+        .style('fill', d => {
+            return color(d.Doping !== '');
+        })
         // Makes changes when mouse hover overs a bar. d is for height
-        .on('mouseover', function (event, d) {
-            var i = this.getAttribute('index');
+        .on('mouseover', (event, d) => {
 
             // Overlay for tooltip 
             overlay

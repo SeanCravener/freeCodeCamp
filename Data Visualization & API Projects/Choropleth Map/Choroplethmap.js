62cbb66e64af6ef30d133d65f8264f3b9767ee3a
Fatgotenkz <99251238+Fatgotenkz@users.noreply.github.com>
2022-10-16 21:05:26 -0400

    updated choroplethmap.js and css

--- Data Visualization & API Projects/Choropleth Map/Choroplethmap.js
+++ Data Visualization & API Projects/Choropleth Map/Choroplethmap.js
@@ -38,10 +38,10 @@ let svg = d3.select(".container")
     .attr("id", "choropleth-map")
 
 
-let legend = svg
-    .append('g')
-    .attr('id', 'legend')
-    .attr('transform', 'translate(0,40)');
+// let legend = svg
+//     .append('g')
+//     .attr('id', 'legend')
+//     .attr('transform', 'translate(0,40)');
   
 
 
@@ -51,11 +51,6 @@ Promise.all([
     d3.json(COUNTY_FILE)
 ]).then((userData, countyData) => {
     
-    // Map of userData.fip from EDUCATION_FILE 
-    const fipsMap = userData.map(d => {
-        return d.fips
-    })
-
     // Map of bachelorOrHigher value
     const bachelorMap = userData.map(d => {
         return d.bachelorsOrHigher;
@@ -66,60 +61,61 @@ Promise.all([
         .scaleSequential(d3.interpolateYlGnBu)
         .domain(d3.extent(bachelorMap));
 
+    // const x = d3    
+    //     .scaleLinear()
+    //     .domain(d3.extent(bachelorMap))
+    //     .rangeRound([600, 860]);
 
-    legend
-        .selectAll('rect')
-        .data(
-            color.range().map( d => {
-                d = color.invertExtent(d);
-                if (d[0] === null) {
-                    d[0] = x.domain()[0];
-                }
-                if (d[1] === null) {
-                    d[1] = x.domain()[1];
-                }
-                return d;
-            })
-        )
-        .enter()
-        .append('rect')
-        .attr('height', 8)
-        .attr('x', (d => {
-            return x(d[0]);
-        }))
-        .attr('width', (d => {
-            return d[0] && d[1] ? x(d[1]) - x(d[0]) : x(null);
-        }))
-        .attr('fill', (d => {
-            return color(d[0]);
-        }));
+    // legend
+    //     .selectAll('rect')
+    //     .data(
+    //         color.range().map( d => {
+    //             d = color.invertExtent(d);
+    //             if (d[0] === null) {
+    //                 d[0] = x.domain()[0];
+    //             }
+    //             if (d[1] === null) {
+    //                 d[1] = x.domain()[1];
+    //             }
+    //             return d;
+    //         })
+    //     )
+    //     .enter()
+    //     .append('rect')
+    //     .attr('height', 8)
+    //     .attr('x', (d => {
+    //         return x(d[0]);
+    //     }))
+    //     .attr('width', (d => {
+    //         return d[0] && d[1] ? x(d[1]) - x(d[0]) : x(null);
+    //     }))
+    //     .attr('fill', (d => {
+    //         return color(d[0]);
+    //     }));
   
-//   g.append('text')
-//     .attr('class', 'caption')
-//     .attr('x', x.range()[0])
-//     .attr('y', -6)
-//     .attr('fill', '#000')
-//     .attr('text-anchor', 'start')
-//     .attr('font-weight', 'bold');
+    // legend.append('text')
+    //     .attr('class', 'caption')
+    //     .attr('x', x.range()[0])
+    //     .attr('y', -6)
+    //     .attr('fill', '#000')
+    //     .attr('text-anchor', 'start')
+    //     .attr('font-weight', 'bold');
   
-legend
-    .call(
-        d3
-        .axisBottom(x)
-        .tickSize(13)
-        .tickFormat(function (x) {
-            return Math.round(x) + '%';
-        })
-        .tickValues(color.domain())
-    )
-    .select('.domain')
-    .remove();
+    // legend.call(
+    //     d3.axisBottom(x)
+    //         .tickSize(13)
+    //         .tickFormat(x => {
+    //             return Math.round(x) + '%';
+    //     })
+    //     .tickValues(color.domain())
+    // )
+    // .select('.domain')
+    // .remove();
 
 
     svg.append("g")
         .selectAll("path")
-        // parses the GeoJSON data into a FeatureCollection data array
-        .data(topojson.feature(us, us.objects.counties).features)
+        .data(topojson.feature(countyData, countyData.objects.counties).features)
         .enter()
         .append("path")
         .attr("class", "county")
@@ -129,38 +125,57 @@ legend
           return object[0] ? object[0].bachelorsOrHigher : 0;
         })
         .attr("fill", d => {
-          let object = education.filter(item => item.fips == d.id);
+          let object = userData.filter(item => item.fips == d.id);
           return object[0] ? color(object[0].bachelorsOrHigher) : color(0);
         })
         .attr("d", path)
         .on("mouseoover", (event, d) => {
-            // Add transition for when Tooltip appears
             tooltip.transition().duration(200).style('opacity', 0.9);
             tooltip
-                .attr("data-education", d.bachelorsOrHigher)
-                .html(
-                    d.Name +
+                .html(() => {
+                    let result = userData.filter(function (obj) {
+                      return obj.fips === d.id;
+                    });
+                    if (result[0]) {
+                      return (
+                        result[0]['area_name'] +
+                        ', ' +
+                        result[0]['state'] +
                         ': ' +
-                        d.Nationality +
-                        '<br/>' +
-                        'Year: ' +
-                        d.Year +
-                        ', Time: ' +
-                        timeFormat(d.Time) +
-                        (d.Doping ? '<br/><br/>' + d.Doping : '')
-                    )
-                    // Placement of tooltip
+                        result[0].bachelorsOrHigher +
+                        '%'
+                      );
+                    }
+                    return 0;
+                })
+                .attr('data-education', function () {
+                    let result = userData.filter(function (obj) {
+                      return obj.fips === d.id;
+                    });
+                    if (result[0]) {
+                      return result[0].bachelorsOrHigher;
+                    }
+                    // could not find a matching fips id in the data
+                    return 0;
+                  })
                     .style('left', event.pageX + 'px')
                     .style('top', event.pageY - 28 + 'px');
-            })
-            // Transition on mouseout event
-            .on('mouseout', () => {
-                tooltip.transition().duration(200).style('opacity', 0);
-            });
+        })
+        // Transition on mouseout event
+        .on('mouseout', () => {
+            tooltip.transition().duration(200).style('opacity', 0);
+        });
 
-
-
-}).catch(error => {
+    svg.append('path')
+        .datum(
+          topojson.mesh(countyData, countyData.objects.states, function (a, b) {
+            return a !== b;
+          })
+        )
+        .attr('class', 'states')
+        .attr('d', path);
+})
+.catch(error => {
     // Catch error and print error to console
     console.log(error);
 })

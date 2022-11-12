471ad628758c37ac22c6679cd3d8677641cd71d5
Sean C <scravenerpat@gmail.com>
2022-11-12 17:04:01 -0500

    Markdown previewer update
    Figuring out the layout using Bootstrap with React

--- Front End Development Libraries/projects/markdown-previewer/App.jsx
+++ Front End Development Libraries/projects/markdown-previewer/App.jsx
@@ -1,2 +1,25 @@
-import React from 'react'
-import './style.css'
+import './App.css';
+
+const App = () => {
+
+    return (
+        <div className='container'>
+            <div className='row text-center'>
+                <div className='col-sm-12 text-center'>
+                <h1>
+                    Markdown Previewer
+                </h1>
+                </div>
+                <div className='col-md-6'>
+                    <textarea type='text' id='editor' value='editor box' style={{height: '500px', width: '500px'}}></textarea>
+                </div>
+                <div className='col-md-6'>
+                    <textarea type='text' id='preview' value='preview box' style={{height: '500px', width: '500px'}} readOnly={true}></textarea>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
+

02d2b187e31206bdae1dc9a44bd7dd4ae95b12b0
Sean C <scravenerpat@gmail.com>
2022-11-14 22:45:16 -0500

    update drum machine project

---
+++ Front End Development Libraries/projects/drum-machine/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+
+const App = () => {
+
+    return (
+        <div className='App border border-primary' >
+        </div>
+    )
+}
+
+export default App
+

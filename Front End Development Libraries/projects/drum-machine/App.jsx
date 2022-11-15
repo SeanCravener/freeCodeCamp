730fc7f1504587e4d26da3c3d029964c8c655721
Fatgotenkz <99251238+Fatgotenkz@users.noreply.github.com>
2022-11-15 18:55:21 -0500

    drum machine update

--- Front End Development Libraries/projects/drum-machine/App.jsx
+++ Front End Development Libraries/projects/drum-machine/App.jsx
@@ -18,8 +18,7 @@ const drumSamples = [
 const App = () => {
 
     return (
-        <div className='App d-flex justify-content-center align-items-center' id='drum-machine'>
-            <div className='row'>
+            <div className='row d-flex justify-content-center align-items-center' id='drum-machine'>
                 <div className='col-6'>
                     <Controls />
                 </div>
@@ -27,7 +26,6 @@ const App = () => {
                     <Drumpads />
                 </div>
             </div>
-        </div>
     )
 }
 
@@ -37,9 +35,9 @@ export default App
 const Drumpads = () => {
 
     return (
-        <div className='row'>
+        <div className='row gap-2 d-flex justify-content-center align-items-center p-1'>
             { drumSamples.map(item => {
-                return ( <button className='drum-pad btn btn-primary col-4' type='button' id={item.key}>
+                return ( <button className='drum-pad btn btn-primary col-3 p-2' type='button' id={item.key}>
                     {item.key}
                 </button> );
             })}
@@ -51,11 +49,12 @@ const Controls = () => {
 
     return (
         <div className='row'>
-            <div className='col-6'>
-                <button className='btn btn-primary'>On</button>
+            <div id='display' className='bg-light'>
+                Snare Drum!
             </div>
-            <div className='col-6'>
-                <button className='btn btn-primary'>Off</button>
+            <div className='row m-1 '>
+                <label for="volume" className="form-label col-12">Volume</label>
+                <input type="range" className="form-range" min="0" max="1" id="volume"></input>
             </div>
         </div>
     )

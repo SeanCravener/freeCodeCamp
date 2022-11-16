7276bd10060e1e3067cee71a07e192f1a42af1cf
Sean C <scravenerpat@gmail.com>
2022-11-15 20:48:58 -0500

    drum machine update
    trying to figure out how I want to handle the events and sounds

--- Front End Development Libraries/projects/drum-machine/App.jsx
+++ Front End Development Libraries/projects/drum-machine/App.jsx
@@ -1,62 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+
 import './style.css';
 
 const drumSamples = [
-    {key: 'Q', label: 'Heater 1', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'},
-    {key: 'W', label: 'Heater 2', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'},
-    {key: 'E', label: 'Heater 3', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'},
-    {key: 'A', label: 'Heater 4', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'},
-    {key: 'S', label: 'Clap', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'},
-    {key: 'D', label: 'Open-HH', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'},
-    {key: 'Z', label: 'Kick-n-Hat', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'},
-    {key: 'X', label: 'Kick', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'},
-    {key: 'C', label: 'Closed-HH', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'}
+    {key: 81, id: 'Q', label: 'Heater 1', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
+    {key: 87, id: 'W', label: 'Heater 2', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
+    {key: 69, id: 'E', label: 'Heater 3', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
+    {key: 65, id: 'A', label: 'Heater 4', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
+    {key: 83, id: 'S', label: 'Clap', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
+    {key: 68, id: 'D', label: 'Open-HH', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
+    {key: 90, id: 'Z', label: 'Kick-n-Hat', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
+    {key: 88, id: 'X', label: 'Kick', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
+    {key: 67, id: 'C', label: 'Closed-HH', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' }
 ]
 
 // Need to create event listener, maybe custom hook
 
 const App = () => {
 
+
+
     return (
-            <div className='row d-flex justify-content-center align-items-center' id='drum-machine'>
-                <div className='col-6'>
-                    <Controls />
+        <div className='row d-flex justify-content-center align-items-center' id='drum-machine'>
+            <div className='col-6'>
+                <div id='display' className='bg-light'>
+                    Snare Drum!
                 </div>
-                <div className='col-6'>
-                    <Drumpads />
+                <label for="volume" className="form-label">Volume</label>
+                <input type="range" className="form-range" min="0" max="1" id="volume"></input>
+            </div>
+            <div className='col-6'>
+                <div className='row gap-2 d-flex justify-content-center align-items-center p-1'>
+                    {drumSamples.map(item => {
+                        return (<button className='drum-pad btn btn-primary col-3 p-2'  type='button' id={item.id}>
+                            {item.id}
+                        </button>);
+                    })}
                 </div>
             </div>
+        </div>
     )
 }
 
 export default App
 
 
-const Drumpads = () => {
-
-    return (
-        <div className='row gap-2 d-flex justify-content-center align-items-center p-1'>
-            { drumSamples.map(item => {
-                return ( <button className='drum-pad btn btn-primary col-3 p-2' type='button' id={item.key}>
-                    {item.key}
-                </button> );
-            })}
-        </div>
-    )
-}
-
-const Controls = () => {
-
-    return (
-        <div className='row'>
-            <div id='display' className='bg-light'>
-                Snare Drum!
-            </div>
-            <div className='row m-1 '>
-                <label for="volume" className="form-label col-12">Volume</label>
-                <input type="range" className="form-range" min="0" max="1" id="volume"></input>
-            </div>
-        </div>
-    )
-}
 

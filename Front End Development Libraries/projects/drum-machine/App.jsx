89fb9f7318139713e50386930b9dd0071708184f
Sean C <scravenerpat@gmail.com>
2022-11-22 00:04:03 -0500

    Setting Up Next Project
    Started setting up folders/files for the next frontend project, a javascript calculator.

--- Front End Development Libraries/projects/drum-machine/App.jsx
+++ Front End Development Libraries/projects/drum-machine/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import './style.css';
 
 const drumSamples = [
     {key: 'Q', label: 'Heater 1', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },

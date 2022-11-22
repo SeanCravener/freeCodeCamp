bdeb835bafa96d2d52afba4f8b07ffeda19afe35
Sean C <scravenerpat@gmail.com>
2022-11-21 19:21:21 -0500

    Drum Machine Finished!
    Finished and turned in drum machine project. Made with React and Bootstrap.

--- Front End Development Libraries/projects/drum-machine/App.jsx
+++ Front End Development Libraries/projects/drum-machine/App.jsx
@@ -1,30 +1,29 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import './style.css';
 
 const drumSamples = [
-    {key: 81, id: 'Q', label: 'Heater 1', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
-    {key: 87, id: 'W', label: 'Heater 2', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
-    {key: 69, id: 'E', label: 'Heater 3', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
-    {key: 65, id: 'A', label: 'Heater 4', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
-    {key: 83, id: 'S', label: 'Clap', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
-    {key: 68, id: 'D', label: 'Open-HH', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
-    {key: 90, id: 'Z', label: 'Kick-n-Hat', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
-    {key: 88, id: 'X', label: 'Kick', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
-    {key: 67, id: 'C', label: 'Closed-HH', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' }
+    {key: 'Q', label: 'Heater 1', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
+    {key: 'W', label: 'Heater 2', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
+    {key: 'E', label: 'Heater 3', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
+    {key: 'A', label: 'Heater 4', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
+    {key: 'S', label: 'Clap', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
+    {key: 'D', label: 'Open-HH', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
+    {key: 'Z', label: 'Kick-n-Hat', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
+    {key: 'X', label: 'Kick', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
+    {key: 'C', label: 'Closed-HH', fileURL: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' }
 ]
 
 const App = () => {
-    const [volume, setVolume] = React.useState(0.50)
-    const [label, setLabel] = React.useState('Play Something!')
+    const [volume, setVolume] = useState(0.50)
+    const [label, setLabel] = useState('Play Something!')
     
-
     return (
         <div className='row d-flex justify-content-center align-items-center' id='drum-machine'>
             <div className='col-6'>
                 <Display label={label} volume={volume} setVolume={setVolume}/>
             </div>
             <div className='col-6'>
-                <div className='row gap-2 d-flex justify-content-center align-items-center p-1'>
+                <div className='row gap-2 p-1'>
                 {drumSamples.map((item) => (
                       <Drumpad drumSound={item} volume={volume} setLabel={setLabel} />
                     ))}
@@ -51,20 +50,30 @@ const Display = ({ label, volume, setVolume }) => {
 }
 
 const Drumpad = ({ drumSound, volume, setLabel }) => {
-    const ref = React.useRef();
-    const handleClick = (audioRef) => {
-        setLabel(audioRef.current.id);
-        audioRef.current.volume = volume;
-        audioRef.current.play();
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyPress)
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress)
+        };
+      }, [volume])
+    
+    const handleKeyPress = (event) => {
+        const key = event.key.toUpperCase()
+        playAudio(key);
+      }
+
+    const playAudio = (key) => {
+        const audioDOM = document.getElementById(key)
+        setLabel(audioDOM.getAttribute('label'))
+        audioDOM.volume = volume;
+        audioDOM.play()
     }
 
     return (
-        <button className='drum-pad btn btn-primary col-3 p-2' type='button' id={drumSound.label} onClick={() => handleClick(ref)}>
-            {drumSound.id}
-            <audio src={drumSound.fileURL} ref={ref} volume={volume} id={drumSound.label}/>
+        <button className='drum-pad btn btn-primary col-3 p-2' type='button' onClick={() => playAudio(drumSound.key)}>
+            {drumSound.key}
+            <audio className='clip' src={drumSound.fileURL} id={drumSound.key} volume={volume} label={drumSound.label}/>
         </button>
     )
 }
-
-
-

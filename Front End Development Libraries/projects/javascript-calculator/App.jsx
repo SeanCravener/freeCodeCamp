07afc70bd49955877fae5483e54aa3cbdc6ddd74
Sean C <scravenerpat@gmail.com>
2022-11-28 20:22:48 -0500

    javascript calculator update
    Set up buttons and display, got input working. Need to set up formula to calculate input.

--- Front End Development Libraries/projects/javascript-calculator/App.jsx
+++ Front End Development Libraries/projects/javascript-calculator/App.jsx
@@ -1,84 +1,145 @@
 import React, { useState } from "react";
 
+
 const App = () => {
-    const [displayValue, setDisplayValue] = useState(0);
-    
+    const [displayValue, setDisplayValue] = useState('Hello');
+    const [equationValue, setEquationValue] = useState(['1', '+', '2']);
+
     return (
-        <div>
-            <Display displayValue={displayValue} />
-            <BtnPad displayValue={displayValue} setDisplayValue={setDisplayValue} />
+        <div className='row'>
+            <Display equationValue={equationValue} displayValue={displayValue} />
+            <BtnPad equationValue={equationValue} setEquationValue={setEquationValue} setDisplayValue={setDisplayValue} />
         </div>
     )
 }
 
-const Display = (displayValue) => {
+const Display = ({ equationValue, displayValue }) => {
 
     return (
-        <div>
-            {displayValue}
+        <div className='row gap-1'>
+            <div className='row'>
+                {equationValue}
+            </div>
+            <div className='row'>
+                {displayValue}
+            </div>
         </div>
     )
 }
 
-const BtnPad = (displayValue, setDisplayValue) => {
+const BtnPad = ({ equationValue, setEquationValue, setDisplayValue }) => {
 
     const handleClick = (btnValue) => {
-        
+        if(btnValue === 'clear') {
+            setEquationValue([])
+            setDisplayValue('You reset this bitch')
+        } else {
+            setEquationValue([...equationValue, btnValue])
+            setDisplayValue(btnValue)
+        }
     }
 
     return (
-        <div>
-            <button id='AC' className='btn btn-primary col-6 p-2' onClick={handleClick()} type='button'>
-                AC
-            </button>
-            <button id='slash' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                /
-            </button>
-            <button id='multiply' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                x
-            </button>
-            <button id='7' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                7
-            </button>
-            <button id='8' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                8
-            </button>
-            <button id='9' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                9
-            </button>
-            <button id='minus' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                -
-            </button>
-            <button id='4' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                4
-            </button>
-            <button id='5' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                5
-            </button>
-            <button id='6' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                6
-            </button>
-            <button id='addition' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                +
-            </button>
-            <button id='1' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                1
-            </button>
-            <button id='2' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                2
-            </button>
-            <button id='3' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                3
-            </button>
-            <button id='equals' className='btn btn-primary col-3 row-2 p-2' onClick={handleClick()} type='button'>
-                =
-            </button>
-            <button id='0' className='btn btn-primary col-6 p-2' onClick={handleClick()} type='button'>
-                0
-            </button>
-            <button id='dot' className='btn btn-primary col-3 p-2' onClick={handleClick()} type='button'>
-                .
-            </button>
+        <div className='row'>
+            <div className='row g-0'>
+                <div className='d-grid col-6 p-1'>
+                    <button id='AC' className='btn btn-primary p-2' onClick={() => handleClick('clear')} type='button'>
+                        AC
+                    </button>
+                </div>
+                <div className='d-grid col-3 p-1'>
+                    <button id='slash' className='btn btn-primary p-2' onClick={() => handleClick('/')} type='button'>
+                        /
+                    </button>
+                </div>
+                <div className='d-grid col-3 p-1'>
+                    <button id='multiply' className='btn btn-primary p-2' onClick={() => handleClick('x')} type='button'>
+                        x
+                    </button>
+                </div>
+            </div>
+            <div className='row g-0'>
+                <div className='d-grid col-3 p-1'>
+                    <button id='7' className='btn btn-primary p-2' onClick={() => handleClick('7')} type='button'>
+                        7
+                    </button>
+                </div>
+                <div className='d-grid col-3 p-1'>
+                    <button id='8' className='btn btn-primary p-2' onClick={() => handleClick('8')} type='button'>
+                        8
+                    </button>
+                </div>
+                <div className='d-grid col-3 p-1'>
+                    <button id='9' className='btn btn-primary p-2' onClick={() => handleClick('9')} type='button'>
+                        9
+                    </button>
+                </div>
+                <div className='d-grid col-3 p-1'>
+                    <button id='minus' className='btn btn-primary p-2' onClick={() => handleClick('-')} type='button'>
+                        -
+                    </button>
+                </div>
+            </div>
+            <div className='row g-0'>
+                <div className='d-grid col-3 p-1'>
+                    <button id='4' className='btn btn-primary p-2' onClick={() => handleClick('4')} type='button'>
+                        4
+                    </button>
+                </div>
+                <div className='d-grid col-3 p-1'>
+                    <button id='5' className='btn btn-primary p-2' onClick={() => handleClick('5')} type='button'>
+                        5
+                    </button>
+                </div>
+                <div className='d-grid col-3 p-1'>
+                    <button id='6' className='btn btn-primary p-2' onClick={() => handleClick('6')} type='button'>
+                        6
+                    </button>
+                </div>
+                <div className='d-grid col-3 p-1'>
+                    <button id='addition' className='btn btn-primary p-2' onClick={() => handleClick('+')} type='button'>
+                        +
+                    </button>
+                </div>
+            </div>
+            <div className='row g-0'>
+                <div className='col-9'>
+                    <div className='row g-0'>
+                        <div className='d-grid col-4 p-1'>
+                            <button id='1' className='btn btn-primary p-2' onClick={() => handleClick('1')} type='button'>
+                                1
+                            </button>
+                        </div>
+                        <div className='d-grid col-4 p-1'>
+                            <button id='2' className='btn btn-primary p-2' onClick={() => handleClick('2')} type='button'>
+                                2
+                            </button>
+                        </div>
+                        <div className='d-grid col-4 p-1'>
+                            <button id='3' className='btn btn-primary p-2' onClick={() => handleClick('3')} type='button'>
+                                3
+                            </button>
+                        </div>
+                    </div>
+                    <div className='row g-0'>
+                        <div className='d-grid col-8 p-1'>
+                            <button id='0' className='btn btn-primary p-2' onClick={() => handleClick('0')} type='button'>
+                                0
+                            </button>
+                        </div>
+                        <div className='d-grid col-4 p-1'>
+                            <button id='dot' className='btn btn-primary p-2' onClick={() => handleClick('.')} type='button'>
+                                .
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='d-grid col-3 p-1'>
+                    <button id='equals' className='btn btn-primary p-2' onClick={() => handleClick('Need to finish equal sign')} type='button'>
+                        =
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }

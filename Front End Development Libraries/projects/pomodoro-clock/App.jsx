742a7a5052a95048b8bfcb9114f2466b339b02e2
Sean C <scravenerpat@gmail.com>
2022-12-07 01:03:38 -0500

    25 + 5 Clock Update
    Started on the last front end project which is a Pomodoro clock (25+5). Already got the increment and decrement buttons working, need to work on timer part next

---
+++ Front End Development Libraries/projects/pomodoro-clock/App.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const INI_BREAK = 5
+const INI_SESSION = 25
+
+const App = () => {
+    const [breakL, setBreakL] = useState(INI_BREAK)
+    const [sessionL, setSessionL] = useState(INI_SESSION)
+
+    const increment = (incrementNum) => {
+        incrementNum += 1
+        if(incrementNum <= 0 || incrementNum > 60) {
+            return
+        }
+        return incrementNum
+    }
+
+    const decrement = (decrementNum) => {
+        decrementNum -= 1
+        if(decrementNum <= 0 || decrementNum > 60) {
+            return
+        }
+        return decrementNum
+    }
+
+    return (
+        <div className='container'>
+            <div className='row'>
+                <span className='fs-2'>
+                    25 + 5 Clock
+                </span>
+            </div>
+            <div className='row'>
+                <div className='col-6'>
+                    <div className='row'>
+                        <div className='col-12 fs-3' id='break-label'>
+                            Break Length
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-4' id='break-decrement'>
+                            <button onClick={() => setBreakL(decrement(breakL))} type='button'>
+                            <FontAwesomeIcon icon="fa-solid fa-circle-down" />
+                            </button>
+                        </div>
+                        <div className='col-4' id='break-length'>
+                            {breakL}
+                        </div>
+                        <div className='col-4' id='break-increment'>
+                        <button onClick={() => setBreakL(increment(breakL))} type='button'>
+                            <FontAwesomeIcon icon="fa-solid fa-circle-up" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-6'>
+                    <span id='session-label' className='fs-3'>
+                        Session Length
+                    </span>
+                    <div className='row'>
+                        <div className='col-4' id='session-decrement'>
+                        <button className='btn btn-primary' onClick={() => setSessionL(decrement(sessionL))} type='button'>
+
+                            {/* <FontAwesomeIcon icon="fa-solid fa-circle-down" /> */}
+                            </button>
+                        </div>
+                        <div className='col-4' id='session-length'>
+                            {sessionL}
+                        </div>
+                        <div className='col-4' id='session-increment'>
+                        <button onClick={() => setSessionL(increment(sessionL))} type='button'>
+                            {/* <FontAwesomeIcon icon="fa-solid fa-circle-up" /> */}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Timer breakTimer={breakL} sessionTimer={sessionL} />
+        </div>
+    )
+}
+
+const Timer = ({breakTimer, sessionTimer}) => {
+
+
+    return (
+        <div className='row'>
+            <div className='col-12'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='row'>
+                            <div className='col-12' id='timer-label'>
+                                {breakTimer}
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col-12' id='time-left'>
+                                {sessionTimer}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-4' id='start_stop'>
+                        {/* <FontAwesomeIcon icon="fa-solid fa-circle-play" /> */}
+                    </div>
+                    <div className='col-4' id='start_stop'>
+                        {/* <FontAwesomeIcon icon="fa-solid fa-circle-pause" /> */}
+                    </div>
+                    <div className='col-4' id='reset'>
+                        {/* <FontAwesomeIcon icon="fa-solid fa-arrows-rotate" /> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
+
